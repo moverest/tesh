5e45788727272a0c1db9a6cb318570f5f68d3abd
Clément Martinez <clementmartinezdev@gmail.com>
2017-12-07 15:06:44 +0100

    Fix tester.sh

--- tester.sh
+++ tester.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 echo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. > tmp-lorem
-for i in tests/*_in; do
-  ./tesh < $i > tmp
-  DIFFF=`diff tmp ${i/_in/_out}`
-  if [ -z "${DIFFF}" ]; then
-    echo "Test ${i/_in/} OK"
-  elif [ -n "${DIFFF}" ]; then
-    echo "Test ${i/_in/} FAILED"
+
+num_successes=0
+num_failures=0
+
+for f in tests/*_in; do
+  test_path=${f/_in/}
+  if diff --color <(./tesh < $f) ${test_path}_out
+  then
+    echo -e "\x1b[32m[PASSED]\x1b[0m $test_path"
+    num_successes=`expr $num_successes + 1`
+  else
+      echo -e "\x1b[31m[FAILED]\x1b[0m $test_path"
+      num_failures=`expr $num_failures + 1`
   fi
 done
 
+echo
+echo -e "\x1b[32m$num_successes succeeded.\x1b[0m"
+echo -e "\x1b[31m$num_failures failed.\x1b[0m"
+
 rm tmp*
+
+[ $num_failures == 0 ]
+
