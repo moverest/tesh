f417e4ad17b82b4565dd68f1f0cf0cc69aa884cc
John KLV <jean.calvet@telecomnancy.net>
2017-12-12 12:43:18 +0100

    WIP Readline part 1

--- src/tesh.c
+++ src/tesh.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <dlfcn.h>
 
 #include "parser.h"
 #include "input_reader.h"
 
+
+#define READLINEMODE    true
+
 int main() {
+    char * (*readline)(char *);
+
+    void (*add_history)(char *);
+
+    if (READLINEMODE) {
+        void *handle = dlopen("libreadline.so", RTLD_LAZY);
+
+        if (!handle) {
+            printf("Erreur au chargement de readline\n");
+            return -1;
+        }
+    }
+
     char     *buffer;
     parser_t *parser;
     int      status_code = 0;
     bool     at_eof      = false;
 
     do {
-        buffer = get_input(stdin, &at_eof);
+        if (READLINEMODE) {
+            buffer = readline(get_prompt());
+            if (buffer[0] != 0) {
+                add_history(buffer);
+            }
+        } else {
+            buffer = get_input(stdin, &at_eof);
+        }
+
         parser = new_parser(buffer);
 
         compound_statement_t *cstatement;
