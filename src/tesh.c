413a9910304beceeff096e6d15f2200df702b1dc
John KLV <jean.calvet@telecomnancy.net>
2017-12-13 03:02:09 +0100

    Format :)

--- src/tesh.c
+++ src/tesh.c
@@ -6,56 +6,59 @@
 #include "parser.h"
 #include "input_reader.h"
 
-int update_flags(int argc, char const* argv[], FILE** fdIn, bool* SCRIPTMOD, bool* READLINEMOD, bool* ERRORMOD){
-  int error;
-  for(int i = 1; i < argc; i++){
-    if(argv[i]!=NULL){
-      if (strlen(argv[i])>=2 && argv[i][0] == '-'){
-        if (strlen(argv[i])==2 && argv[i][1] == 'r'){
-          //printf("MODE READLINE\n");
-          *READLINEMOD = true;
-          error += 1;
-        } else if (strlen(argv[i])==2 && argv[i][1] == 'e') {
-          //printf("MODE QUIT_ON_ERROR\n");
-          *ERRORMOD = true;
-          error += 2;
+int update_flags(int argc, char const *argv[], FILE **fdIn, bool *SCRIPTMOD, bool *READLINEMOD, bool *ERRORMOD) {
+    int error;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i] != NULL) {
+            if ((strlen(argv[i]) >= 2) && (argv[i][0] == '-')) {
+                if ((strlen(argv[i]) == 2) && (argv[i][1] == 'r')) {
+                    //printf("MODE READLINE\n");
+                    *READLINEMOD = true;
+                    error       += 1;
+                } else if ((strlen(argv[i]) == 2) && (argv[i][1] == 'e')) {
+                    //printf("MODE QUIT_ON_ERROR\n");
+                    *ERRORMOD = true;
+                    error    += 2;
+                } else {
+                    printf("NOT RECOGNIZED OPTION GIVEN.\n");
+                    error -= 999;
+                }
+            } else {
+                //printf("MODE READ FROM SCRIPT FILE\n");
+                *SCRIPTMOD = true;
+                *fdIn      = fopen(argv[i], "r");
+                error     += 4;
+            }
         } else {
-          printf("NOT RECOGNIZED OPTION GIVEN.\n");
-          error-=999;
+            printf("NOT RECOGNIZED ARG GIVEN.\n");
+            error -= 999;
         }
-      } else {
-        //printf("MODE READ FROM SCRIPT FILE\n");
-        *SCRIPTMOD = true;
-        *fdIn = fopen(argv[i], "r");
-        error += 4 ;
-      }
-    } else {
-      printf("NOT RECOGNIZED ARG GIVEN.\n");
-      error-=999;
     }
-  }
-  return error ;
+    return error;
 }
+
 
 int main(int argc, char const *argv[]) {
     char *(*readline)(char *);
+
     void (*add_history)(char *);
     void *handle;
-    FILE* fdIn = stdin ;
-    bool READLINEMOD = false ;
-    bool ERRORMOD = false ;
-    bool SCRIPTMOD = false ;
-    int err = 0 ;
+    FILE *fdIn       = stdin;
+    bool READLINEMOD = false;
+    bool ERRORMOD    = false;
+    bool SCRIPTMOD   = false;
+    int  err         = 0;
 
     err = update_flags(argc, argv, &fdIn, &SCRIPTMOD, &READLINEMOD, &ERRORMOD);
-    if(err<0){
-      printf("ERROR WHILE PARSING ARGV\n");
-      return -1;
+    if (err < 0) {
+        printf("ERROR WHILE PARSING ARGV\n");
+        return -1;
     }
 
-    if(READLINEMOD && !isatty(0)){
-      printf("ERROR : CAN'T READ BOTH FROM SCRIPT AND READLINE INPUT\n");
-      return -1;
+    if (READLINEMOD && !isatty(0)) {
+        printf("ERROR : CAN'T READ BOTH FROM SCRIPT AND READLINE INPUT\n");
+        return -1;
     }
 
     if (READLINEMOD) {
@@ -66,8 +69,8 @@ int main(int argc, char const *argv[]) {
             return -1;
         }
 
-        readline = (char *(*)(char *)) dlsym(handle, "readline");
-        add_history = (void(*)(char *)) dlsym(handle, "add_history");
+        readline    = (char *(*)(char *))dlsym(handle, "readline");
+        add_history = (void (*)(char *))dlsym(handle, "add_history");
     }
 
     char     *buffer;
@@ -95,8 +98,8 @@ int main(int argc, char const *argv[]) {
         free(buffer);
     } while (!at_eof);
 
-    if(SCRIPTMOD){
-      fclose(fdIn);
+    if (SCRIPTMOD) {
+        fclose(fdIn);
     }
 
     return status_code;
