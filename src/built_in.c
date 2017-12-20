aef5069bbcb83055da9bfeb4bf395f6b7d3a4603
John KLV <jean.calvet@telecomnancy.net>
2017-12-20 02:22:18 +0100

    Beginning of built_ins - ep 2

--- src/built_in.c
+++ src/built_in.c
@@ -1,21 +1,46 @@
+#include <stdio.h>
 #include "built_in.h"
 
-built_ins_t* get_built_ins (){
+built_ins_t *get_built_ins() {
+    built_in_t *built_ins = (built_in_t *)malloc(sizeof(built_in_t) * 3);
 
-  built_in_t* built_ins = (built_in_t*) malloc (sizeof(built_in_t)*3);
-
-  built_ins[0] = {"cd", build_in_cd} ;
-  built_ins[1] = {"fg", build_in_fg} ;
-  built_ins[2] = {"ex", build_in_ex} ;
+    built_ins[0] = { "cd", build_in_cd };
+    built_ins[1] = { "fg", build_in_fg };
+    built_ins[2] = { "ex", build_in_ex };
 
 
-  built_ins_t* bi = (built_ins_t*) malloc(sizeof(built_ins_t));
-  bi->built_in = built_ins ;
-  /* = {
-      {"cd", build_in_cd},
-      {"ex", build_in_ex},
-      {"fg", build_in_fg}
-    };
-  */
-  return bi ;
+    built_ins_t *bi = (built_ins_t *)malloc(sizeof(built_ins_t));
+    bi->built_in = built_ins;
+
+    /* = {
+     *  {"cd", build_in_cd},
+     *  {"ex", build_in_ex},
+     *  {"fg", build_in_fg}
+     * };
+     */
+    return bi;
+}
+
+
+/*
+ * cd :
+ * ~ -> go to home (getenv("HOME"))
+ *  -> go to home (//)
+ * path -> go to path
+ *
+ * return 0
+ */
+//exit : exit
+int build_in_cd(command_t *cmd) {
+    int ret;
+
+    if ((cmd->argv[1] == NULL) || (cmd->argv[1][0] == '~')) {
+        ret = chdir(getenv("HOME"));
+    } else {
+        ret = chdir(getenv("HOME"));
+    }
+    if (ret == -1) {
+        printf("built_in.c:~35 OMG WTF BBQ");
+    }
+    return 0;
 }
--- src/built_in.h
+++ src/built_in.h
@@ -4,16 +4,16 @@
 #include "parser.h"
 
 typedef struct {
-  char* name;
-  int (*func) (command_t* cmd);
-} built_in_t ;
+    char *name;
+    int (*func)(command_t *cmd);
+} built_in_t;
 
 typedef struct {
-  built_in_t* built_in;
-} built_ins_t ;
+    built_in_t *built_in;
+} built_ins_t;
 
-int build_in_cd(command_t* cmd);
-int build_in_fg(command_t* cmd);
-int build_in_ex(command_t* cmd);
+int build_in_cd(command_t *cmd);
+int build_in_fg(command_t *cmd);
+int build_in_ex(command_t *cmd);
 
 #endif
